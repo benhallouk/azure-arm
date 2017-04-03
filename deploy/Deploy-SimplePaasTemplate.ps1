9d2be5dce5ae4f14280aee44882de477347b794e
Amine Benhallouk <benhallouk@gmail.com>
2017-04-03 15:11:03 +0100

    initial commit

---
+++ deploy/Deploy-SimplePaasTemplate.ps1
@@ -0,0 +1,33 @@
+﻿### Define variables
+{
+Clear;
+$location = 'North Europe'
+$resourceGroupName = 'org-arm-paas'
+$resourceDeploymentName = 'org-arm-paas-template-deployment'
+$templatePath = 'C:\Workspace\azure-arm\deploy'
+$templateFile = 'simplePaas.json'
+$template = $templatePath + '\' + $templateFile
+}
+
+### Create Resource Group
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $Location `
+    -Verbose -Force
+}
+
+### Deploy Resources
+{
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -Verbose -Force
+}
+
+### Remove the test resource group
+{
+Remove-AzureRmResourceGroup `
+    -Name $resourceGroupName
+}
